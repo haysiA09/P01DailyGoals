f577d8f6eff3ec08eaf5ceff3941d28127d70571
haysiA09 <s9907946j>
2019-04-19 00:12:34 +0800

    ghfvug

--- app/src/main/java/com/example/p01_dailygoals/summaryActivity.java
+++ app/src/main/java/com/example/p01_dailygoals/summaryActivity.java
@@ -1,7 +1,9 @@
 package com.example.p01_dailygoals;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class summaryActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class summaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        // Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        // Get the String array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        // Get the TextView object
+        TextView tv1 = findViewById(R.id.textView);
+        // Display the name and age on the TextView
+        tv1.setText("@string/instructions1 " + info[0] + "\n @string/instructions3 " + info[1] + "\n @string/instructions4"+info[2]
+        +"\n @string/instructions5"+info[3]);
+
     }
 }
